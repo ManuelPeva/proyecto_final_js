bc5fdf6ad9340126810dbf938478583a64ecb19e
manuel_859 <manuel_859@outlook.es>
2023-05-17 07:24:07 -0600

    Finalización de estilos

---
+++ js/signup.js
@@ -0,0 +1,7 @@
+const signupForm = document.querySelector('#signupForm')
+signupForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
+})
